55573bf75662a4fc237c497b3d993d65dd5ffc89
Oleh Vakarchuk <oleh.vakarchuk@FTR.com>
2023-07-12 11:00:15 +0300

    Setup Weather Application

---
+++ README.md
@@ -0,0 +1,17 @@
+# WeatherApp
+
+## Start Application
+
+1. yarn install
+2. cd ios && pod install && cd ..
+3. yarn ios
+
+## You should have
+
+- NodeJS version 18
+- cocoapods 1.12
+- yarn
+
+## Technologies
+
+React, React-Native, NodeJS, react-query, prettier, eslint
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const BASE_URI = 'https://api.weatherapi.com/v1';
+
+export const API_KEY = '05dbcf87751747d49bd145645231007';
+
+export const CURRENT_CITY_INDEX = '@CurrentCityIndex';
+export const CURRENT_CITY = '@CurrentCity';
+export const LIST_CITIES = '@ListCities';
