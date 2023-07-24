9448c534f259ecfac68db25a1b6992aa054434ff
Oleh Vakarchuk <oleh.vakarchuk@FTR.com>
2023-07-24 22:12:43 +0300

    change API_KEY

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const BASE_URI = 'https://api.weatherapi.com/v1';
 
-export const API_KEY = '05dbcf87751747d49bd145645231007';
+export const API_KEY = 'bf46ea18ec4f45dd90e191014232407';
 
 export const CURRENT_CITY_INDEX = '@CurrentCityIndex';
 export const CURRENT_CITY = '@CurrentCity';
